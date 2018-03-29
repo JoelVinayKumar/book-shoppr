32632ac4736476c5a55ec514be72839bf28121f2
Joel <lavotivinay@gmail.com>
2018-03-29 18:33:50 +0530

    Updated logout and clear cart feature

--- book_shoppe.py
+++ book_shoppe.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, session, redirect, url_for, abort, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_material import Material
+from sqlalchemy import func
 import datetime
 
 from sqlalchemy import Table
@@ -13,7 +14,7 @@ app.config['DEBUG'] = True
 app.config['SECRET_KEY'] ='super-secret-key'
 app.config['USERNAME'] = 'admin'
 app.config['PASSWORD'] = '12345'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///booksmart.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookshoppe.db'
 
 db=SQLAlchemy(app)
 
@@ -27,7 +28,7 @@ class Login(db.Model):
         self.password = password
 
     def __repr__(self):
-        return '<Entry %r %r %r %r>' % (self.email, self.password,)
+        return '<Entry %r %r>' % (self.email, self.password,)
 
 class CustOrders(db.Model):
     # __tablename__ = 'users'
@@ -54,19 +55,19 @@ class CurrentCart(db.Model):
     # __tablename__ = 'users'
     serial = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), db.ForeignKey('login.email'))
-    isbn = db.Column(db.String(20))
+    book = db.Column(db.String(20))
     quantity = db.Column(db.Integer)
     price = db.Column(db.Integer)
 
-    def __init__(self, email, datetime, isbn, quantity, price):
+    def __init__(self, email, datetime, book, quantity, price):
         self.email = email
-        self.isbn = isbn
+        self.book = book
         self.quantity = quantity
         self.price = price
 
     def __repr__(self):
         return '<Entry %r %r %r %r>' % (
-        self.email, self.isbn, self.quantity, self.price)
+        self.email, self.book, self.quantity, self.price)
 
 # Create the table
 db.create_all()
@@ -74,7 +75,7 @@ db.create_all()
 
 def authenticate(e, p):
     print(e)
-    details=Login.query.filter_by(email=e).filter_by(password=p).all()
+    details= Login.query.filter_by(email=e).filter_by(password=p).all()
     print(details)
     if(len(details)>0):
         return True
@@ -83,7 +84,25 @@ def authenticate(e, p):
 
 @app.route('/')
 def homepage():
-    return render_template('homepage.html', size=len(data['items']), data= data)
+    count=0
+    return render_template('homepage.html', size=len(data['items']), data= data, currentBook=count)
+
+@app.route('/logout')
+def logout():
+    if session['logged_in'] == True:
+        session['logged_in'] = False
+        return render_template('homepage.html',size=len(data['items']), data= data)
+    else:
+        return render_template('homepage.html',size=len(data['items']), data= data)
+
+@app.route('/sign_up')
+def signup():
+    return render_template('sign_up.html')
+
+
+@app.route('/show_books')
+def show_books():
+    return render_template('show_books.html', size=len(data['items']), data= data)
 
 @app.route('/book/<int:id>/', methods=['GET','POST'])
 def book(id):
@@ -92,9 +111,9 @@ def book(id):
         return render_template('book.html', id = id, data=data, flag=flag)
     else:
         quantity=request.form.get('comp_select')
-        isbn = 'ISBN' + str(data['items'][id]["volumeInfo"]["industryIdentifiers"][0]["identifier"])
-        cart=CurrentCart(email=session['log_email'], datetime=datetime.datetime.now(),isbn=isbn , quantity=quantity, price=data['items'][id]["saleInfo"]["listPrice"]["amount"])
-        detail = CurrentCart.query.filter_by(isbn=isbn).all()
+        book = str(data['items'][id]["volumeInfo"]["authors"][0])
+        cart=CurrentCart(email=session['log_email'], datetime=datetime.datetime.now(),book=book , quantity=quantity, price=data['items'][id]["saleInfo"]["listPrice"]["amount"])
+        detail = CurrentCart.query.filter_by(book=book).all()
         sum=0
         for d in detail:
             sum+=(d.__dict__["quantity"])
@@ -112,7 +131,6 @@ def book(id):
 def login():
     error = None
     if request.method == 'POST':
-
         if(authenticate(request.form['username'], request.form['password'])):
             session['logged_in'] = True
             session['log_email'] = request.form['username']
@@ -122,12 +140,23 @@ def login():
             error='Invalid credentials'
     return render_template('login.html', error=error)
 
-@app.route('/cart',methods=['GET'])
+@app.route('/cart',methods=['GET','POST'])
 def cart():
-    # lst = CurrentCart.query.filter_by(isbn=isbn).all()
-    # print(lst)
-    return render_template('cart.html',size=len(data['items']), data=data)
+    # lst = CurrentCart.query.filter_by(label=price).all()
+    # total=0
+    # for t in lst:
+    #     total=total+t
+    return render_template('cart.html',size=len(data['items']), data=data, items= CurrentCart.query.all())
 
+@app.route('/clear_cart',methods=['GET'])
+def clear_cart():
+    total=0
+    cart_items= CurrentCart.query.all()
+    for cart_item in cart_items:
+        db.session.delete(cart_item)
+        db.session.commit()
+    flash("You just cleared the cart!!")
+    return render_template('cart.html',size=len(data['items']), data=data, items= CurrentCart.query.all(),total=total)
 
 with open("json/catalog.json") as data_file:
     data = json.loads(data_file.read())
